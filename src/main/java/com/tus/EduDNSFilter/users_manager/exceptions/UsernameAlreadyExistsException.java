c7d6083b9f4ae58e61a8932e9eeeb91e2b481c0e
Mohamad Abo Haileh <a00280341@student.tus.ie>
2025-03-08 19:28:48 +0000

    removed unused exception

--- src/main/java/com/tus/EduDNSFilter/users_manager/exceptions/UsernameAlreadyExistsException.java
+++
@@ -1,9 +0,0 @@
-
-
-package com.tus.EduDNSFilter.users_manager.exceptions;
-
-public class UsernameAlreadyExistsException extends RuntimeException {
-    public UsernameAlreadyExistsException(String message) {
-        super(message);
-    }
-}
